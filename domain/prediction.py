edc9b6b34ac4724c97a12ed09ea51343112d90ae
zucchivan <zucchivan@gmail.com>
2019-12-11 22:34:16 -0300

    Refactors Prediction domain entity

--- domain/prediction.py
+++ domain/prediction.py
@@ -1,7 +1,16 @@
 import datetime
 from dateutil.parser import parse
 
-from domain.city import City
+
+class Prediction:
+    def __init__(self, city_id: int, prediction_date: datetime, rain_probability: int, rain_precipitation: int,
+                 temperature_min: int, temperature_max: int):
+        self.city_id = city_id
+        self.prediction_date = prediction_date
+        self.rain_probability = rain_probability
+        self.rain_precipitation = rain_precipitation
+        self.temperature_min = temperature_min
+        self.temperature_max = temperature_max
 
 
 def get_predictions_from_response_data(response: object):
@@ -13,16 +22,5 @@ def get_predictions_from_response_data(response: object):
         prediction = Prediction(response.get('id'), parse(p.get('date')),
                                 rain.get('probability'), rain.get('precipitation'),
                                 temperature.get('min'), temperature.get('max'))
-        predictions.append(prediction);
+        predictions.append(prediction)
     return predictions
-
-
-class Prediction:
-    def __init__(self, city_id: int, prediction_date: datetime, rain_probability: int, rain_precipitation: int,
-                 temperature_min: int, temperature_max: int):
-        self.city_id = city_id
-        self.prediction_date = prediction_date
-        self.rain_probability = rain_probability
-        self.rain_precipitation = rain_precipitation
-        self.temperature_min = temperature_min
-        self.temperature_max = temperature_max
