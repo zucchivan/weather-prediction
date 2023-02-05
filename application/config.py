0f538afc5717249e1ab3189a45e3d3c21128d8ad
Ivan Zucchi dos Santos <zucchivan@gmail.com>
2023-02-05 22:04:01 +0000

    Moves secret key to env variable

--- application/config.py
+++ application/config.py
@@ -1,6 +1,8 @@
+from os import environ as env
+
 class BaseConfig(object):
     ORIGINS = ["*"]
-    SECRET_KEY = '4)-.W\xad\x80\x97`\x8e\xc1\xcd\x10\xd7\x11\xd6\x00\xf7M\x89\x18\xceCg'
+    SECRET_KEY = env['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CLIMATEMPO_TOKEN = "b22460a8b91ac5f1d48f5b7029891b53"
